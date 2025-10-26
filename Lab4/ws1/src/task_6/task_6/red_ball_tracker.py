8352fef8918e148385f5aec4a4f9d8c8533c47d2
Phillipp <pgery@purdue.edu>
2025-10-26 10:21:07 -0400

    update

--- Lab4/ws1/src/task_6/task_6/red_ball_tracker.py
+++ Lab4/ws1/src/task_6/task_6/red_ball_tracker.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from vision_msgs.msg import BoundingBox2D
 from geometry_msgs.msg import Pose2D
+from geometry_msgs.msg import Twist
 
 
 class ObjectDetector(Node):
@@ -17,23 +18,19 @@ class ObjectDetector(Node):
         
         # Publish bounding box data
         self.bbox_publisher = self.create_publisher(BoundingBox2D, '/bbox', 10)
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
             
         self.bridge = CvBridge()
 
+        self.get_logger().info("Red Ball follower  started.")
+
     def listener_callback(self, msg):
+
         # Convert ROS Image message to OpenCV image
         cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         
-        # --- Object Detection Logic ---
-        # Convert BGR to HSV color space
         hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
         
-        # # for tuning use hsv_tuner.py
-        # lower_bound = np.array([H_min, S_min, V_min])
-        # upper_bound = np.array([H_max, S_max, V_max])
-        
-        # mask = cv2.inRange(hsv_image, lower_bound, upper_bound)
-
         lower_red = np.array([0, 100, 100])
         upper_red = np.array([10, 255, 255])
         mask1 = cv2.inRange(hsv_image, lower_red, upper_red)
@@ -53,25 +50,33 @@ class ObjectDetector(Node):
             
             #  box coordinates
             x, y, w, h = cv2.boundingRect(largest_contour)
-            
-            # center
-            centroid_x = x + w // 2
-            centroid_y = y + h // 2
 
-            self.get_logger().info(f"Object Centroid: (x={centroid_x}, y={centroid_y}), Size: (w={w}, h={h})")
-            
-            bbox_msg = BoundingBox2D()
-            
-            bbox_msg.center.position.x = float(centroid_x)
-            bbox_msg.center.position.y = float(centroid_y)
-            bbox_msg.center.theta = 0.0
+            if w < 10:
+                self.get_logger().info("Object too small, ignoring --> Noicy misstake.")
+                return
+            else:
+                # center
+                centroid_x = x + w // 2
+                centroid_y = y + h // 2
 
-            bbox_msg.size_x = float(w)
-            bbox_msg.size_y = float(h)
-            
-            self.bbox_publisher.publish(bbox_msg)
-            
-            cv2.rectangle(cv_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                image_center_x = cv_image.shape[1] // 2
+
+                #create bounding box as done in previous task_5
+                self.get_logger().info(f"Object Centroid: (x={centroid_x}, y={centroid_y}), Size: (w={w}, h={h})")
+                bbox_msg = BoundingBox2D()                
+                bbox_msg.center.position.x = float(centroid_x)
+                bbox_msg.center.position.y = float(centroid_y)
+                bbox_msg.center.theta = 0.0
+                bbox_msg.size_x = float(w)
+                bbox_msg.size_y = float(h)                
+                self.bbox_publisher.publish(bbox_msg)                
+                cv2.rectangle(cv_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                # Control logic
+                distance_to_object = 
+
+                error_x = centroid_x - image_center_x
+                error_z = 
         
         # Display vieo
         cv2.imshow("Object Detector", cv_image)
